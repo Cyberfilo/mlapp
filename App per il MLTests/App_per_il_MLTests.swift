ce86a1ca81e571955c313378513c38191144fc5a
menghi filippo <filo.gametech@gmail.com>
2024-04-06 23:21:50 +0200

    Initial Commit

---
+++ App per il MLTests/App_per_il_MLTests.swift
@@ -0,0 +1,36 @@
+//
+//  App_per_il_MLTests.swift
+//  App per il MLTests
+//
+//  Created by Filippo Mattia Menghi on 06/07/23.
+//
+
+import XCTest
+@testable import App_per_il_ML
+
+final class App_per_il_MLTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
